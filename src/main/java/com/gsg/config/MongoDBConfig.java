ac01263880bd7ebd3e1e43e18ce0a980cac4f14e
Rajendra sahoo <rajendrasahoodbpb@gmail.com>
2018-12-23 19:40:32 +0530

    - local set up configuration

--- src/main/java/com/gsg/config/MongoDBConfig.java
+++ src/main/java/com/gsg/config/MongoDBConfig.java
@@ -1,15 +1,29 @@
 package com.gsg.config;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.convert.CustomConversions;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 
 @Configuration
 @EnableMongoRepositories(basePackages = { "com.gsg.mongo.repository" },mongoTemplateRef="mongoTemplate")
 @EnableMongoAuditing(modifyOnCreate=true)
 public class MongoDBConfig {
 
-/*	@Bean
+	@Bean
 	public MappingMongoConverter mappingMongoConverter(MongoDbFactory factory, MongoMappingContext mctx,
 			BeanFactory beanFactory) {
 
@@ -23,18 +37,19 @@ public class MongoDBConfig {
 		mappingMongoConverter.setTypeMapper(new DefaultMongoTypeMapper(null));
 		return mappingMongoConverter;
 	}
-*/
+
 	
-	 /*public @Bean MongoClient mongoClient() {
+	 public @Bean MongoClient mongoClient() {
 	       return new MongoClient(
 	    		   //Collections.singletonList(new ServerAddress("localhost", 27017)),
 	    		   //Collections.singletonList(MongoCredential.createScramSha1Credential("myUserAdmin", "gsg", "abc123".toCharArray())));
-	    		   //new MongoClientURI("mongodb://gsgadmin:gsgadmin@ds131137.mlab.com:31137/gsg"));
-	    		   new MongoClientURI("mongodb://localhost:27017/gsg"));
+//	    		   new MongoClientURI("mongodb://gsgadmin:gsgadmin@ds131137.mlab.com:31137/gsg")); // prod
+	    		   new MongoClientURI("mongodb://localhost:27017/")); // local
 	       
 	   }
 	 
 	 public @Bean MongoTemplate mongoTemplate() {
-	      return new MongoTemplate(mongoClient(), "gsg");
-	  }*/
+	      //return new MongoTemplate(mongoClient(), "gsg"); // prod
+		 return new MongoTemplate(mongoClient(), "gsg-test"); // local
+	  }
 }
